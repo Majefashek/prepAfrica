d9ff8fb1fc0af10dbecf1a44be098d5e5ef0a235
unknown <abdullahishuaibumaje.com>
2023-06-27 07:46:33 -0700

    alkl file

--- my_prepAfrica/authentication_app/models.py
+++ my_prepAfrica/authentication_app/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import Group, Permission  # Import additional models
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def __init__(self, model=None):
+        super().__init__()
+        self.model = model
+    
+    def create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user =self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
