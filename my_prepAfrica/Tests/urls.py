d8d80b2637680f93b3e2e7a393ad2b84d76051c8
unknown <abdullahishuaibumaje.com>
2023-07-13 02:22:35 -0700

    my final changes in test

--- my_prepAfrica/Tests/urls.py
+++ my_prepAfrica/Tests/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TestEvaluationView,MyTestView
+from .views import TestEvaluationView,MyTestView,ViewTestResult
 
 urlpatterns = [
     path('test/<int:test_id>/evaluate/', TestEvaluationView.as_view(), name='evaluate_test'),
-    path('mytest',MyTestView.as_view(),name="mytest")
+    path('mytest',MyTestView.as_view(),name="mytest"),
     #path('test/', TestEvaluationView.as_view(), name='evaluate_test'),
+    path('viewtest/<int:test_id>/',ViewTestResult.as_view(),name="viewtest")
 ]
--- my_prepAfrica/Tests/views.py
+++ my_prepAfrica/Tests/views.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 from django.shortcuts import get_object_or_404, render,redirect
 from django.core.paginator import Paginator
 from django.views import View
+from django.http import JsonResponse
+import json
+
 
 
 class MyTestView(View):
@@ -36,44 +39,31 @@ class TestEvaluationView(View):
 
 
     def post(self, request, test_id):
-        #return render(request, 'Tests/evaluate_test.html')
-        
-        question_id = request.POST.get('question_id')
-        selected_option_id = request.POST.get('selected_option')
-        request.session.setdefault('answers', {})[question_id] = selected_option_id
-
         test = get_object_or_404(Test, id=test_id)
         questions = Question.objects.filter(test=test).order_by('id')
         total_questions = questions.count()
-        paginator = Paginator(questions, 1)
-        page_number = request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        answers = request.session.get('answers')
-        if page_obj.has_next():
-            next_page_number = page_obj.next_page_number()
-            redirect_url = reverse('evaluate_test', args=[test_id]) + f'?page={next_page_number}'
-            return redirect(redirect_url)
-
-
-        correct_answers = sum(
-            1 for question_id, selected_option_id in answers.items()
-            if Question.objects.filter(
-                id=question_id, options__id=selected_option_id, options__is_correct=True
-            ).exists()
-        )
-        wrong_answers = total_questions - correct_answers
-
-        
+        data = json.loads(request.body)
+        answers = data.get('answers', {})
+        correct_options = Option.objects.filter(question__test_id=test_id, is_correct=True)
+        correct_answers=0
+        skipped=0
+        wrong_answers=0
+        for question_id, selected_option_id in answers.items():
+            # Check if the selected option is correct for the respective question
+            if correct_options.filter(question_id=question_id, id=selected_option_id).exists():
+                correct_answers += 1
+            elif answers[question_id]=='':
+                skipped+=1
+            else:
+                wrong_answers+=1
         score = (correct_answers / total_questions) * 100
         test.score = score
         test.correct_answers = correct_answers
         test.wrong_answers = wrong_answers
-        test.save()
-
-        context = {
-            'test': test,
-            'page_obj': page_obj,
-            'total_questions': total_questions,
-            'incorrect': wrong_answers}
-
-        return render(request, 'Tests/evaluate_test.html', context)
+        test.save()  
+        return JsonResponse({'testId':test_id})    
+          
+class ViewTestResult(View):
+    def get(self,request,test_id):
+        test=get_object_or_404(Test,id=test_id)
+        return render(request, 'Tests/evaluate_test.html',{'test':test})
