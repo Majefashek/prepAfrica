2bfb9e849e6882322eb35a169fbf0331de479aa5
unknown <abdullahishuaibumaje.com>
2023-07-15 08:57:46 -0700

    added apis

--- my_prepAfrica/my_prepAfrica/urls.py
+++ my_prepAfrica/my_prepAfrica/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core_app.urls')),
-    path('auth/', include('authentication_app.urls')),
-    path('subjects/',include('SubjectSelection.urls')),
-    path('tests/',include('Tests.urls')),
+    path('api/v1/', include('core_app.urls')),
+    path('api/auth/v1/', include('authentication_app.urls')),
+    path('api/subjects/v1/',include('SubjectSelection.urls')),
+    path('api/tests/v1/',include('Tests.urls')),
 ]
